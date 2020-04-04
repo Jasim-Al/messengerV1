1e208d0778c49aabfebe8155809f490df2fdb81e
Jasim-Al <59736291+Jasim-Al@users.noreply.github.com>
2020-04-04 13:18:46 +0530

    SETUP STARTING TEMPLATE

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SendMessage from './SendMessage';
+
+const App = () => {
+  return (
+    <div>
+      <SendMessage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SendMessage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class SendMessage extends Component {
+
+    render() {
+        return (
+            <div>
+                <form action="">
+                    <input type="text"/>
+                    <button>SEND</button>
+                </form>
+            </div>
+        );
+    }
+}
